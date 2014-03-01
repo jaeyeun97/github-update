b0b3d18417ee866847518939ffa5a12543cd7b4c
윤재연 <jyoon16@nmhschool.org>
2014-03-01 00:20:39 -0500

    update.py

---
+++ update.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from flask import request
+from gevent.pywsgi import WSGIServer
+import git
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def main():
+    return "Invalid Request"
+
+
+@app.route('/<repo_name>', methods=['GET', 'POST'])
+def update_repo(repo_name):
+    if request.method == 'POST':
+        repo_path = "/srv/dev/" + repo_name
+        try:
+            repo = git.Repo(repo_path)
+            repo.git.pull()
+            return "Successful"
+        except git.errors.NoSuchPathError:
+            git.Git().clone("git@github.com:jaeyeun97/"+repo_name+".git", repo_path);
+            repo = git.Repo("/srv/dev/"+repo_name)
+            repo.git.pull()
+            return "New repository Cloned"
+    else:
+        return "nothing from "+repo
+
+WSGIServer(('', 8000), app).serve_forever()
