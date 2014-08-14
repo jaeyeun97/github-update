fe0955f12fbbaa6266ec62ef85e629aa39688663
윤재연 <jaeyeun@printinpeace.io>
2014-08-14 11:42:48 +0900

    update

--- update.py
+++ update.py
@@ -2,9 +2,11 @@ from flask import Flask
 from flask import request
 from gevent.pywsgi import WSGIServer
 import git
+import os
 
 app = Flask(__name__)
 
+BASE_DIR = "./"
 
 @app.route('/')
 def main():
@@ -14,17 +16,17 @@ def main():
 @app.route('/<repo_name>', methods=['GET', 'POST'])
 def update_repo(repo_name):
     if request.method == 'POST':
-        repo_path = "/srv/dev/" + repo_name
+        repo_path = os.path.join(BASE_DIR, repo_name)
         try:
             repo = git.Repo(repo_path)
             repo.git.pull()
             return "Successful"
         except git.errors.NoSuchPathError:
-            git.Git().clone("https://github.com/jaeyeun97/"+repo_name+".git", repo_path);
-            repo = git.Repo("/srv/dev/"+repo_name)
+            git.Git().clone("github:jaeyeun97/"+repo_name+".git", repo_path);
+            repo = git.Repo(repo_path)
             repo.git.pull()
             return "New repository Cloned"
     else:
-        return "nothing from "+repo
+        return "<h1> Invaild Access </h1>"
 
 WSGIServer(('', 8000), app).serve_forever()
